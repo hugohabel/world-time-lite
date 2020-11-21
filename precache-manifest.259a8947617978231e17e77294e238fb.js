33775719d79124e4698316b99a41a17e14f5971a
Hugo Habel <hugohabel@gmail.com>
2020-11-21 16:30:46 -0600

    Updates

---
+++ precache-manifest.259a8947617978231e17e77294e238fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39c1b4ba2e9c190b5724bdd1e7634ff5",
+    "url": "/world-time-lite/index.html"
+  },
+  {
+    "revision": "53dcab448d0bde1bb81b",
+    "url": "/world-time-lite/static/css/main.e39376d8.chunk.css"
+  },
+  {
+    "revision": "24183f0eb29ed64630b2",
+    "url": "/world-time-lite/static/js/2.c6842ca3.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/world-time-lite/static/js/2.c6842ca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53dcab448d0bde1bb81b",
+    "url": "/world-time-lite/static/js/main.ef1b9ad7.chunk.js"
+  },
+  {
+    "revision": "24f48056e08b03d49bc0",
+    "url": "/world-time-lite/static/js/runtime-main.8d3d77d3.js"
+  }
+]);
