57aa7bb210a23c9801293ebd5e7078e6e8c21318
Hugo Habel <hugohabel@gmail.com>
2020-11-21 15:29:41 -0600

    Integrate component outside Storybook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 // External Dependencies
 import React from 'react';
 
+// Custom Dependencies
+import WorldTimeWidget from './components/WorldTimeWidget/WorldTimeWidget';
+
 // Styles
 import './App.css';
 
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <WorldTimeWidget />
+    </div>
   );
 }
 
